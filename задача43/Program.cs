edead234bf58d81f3906a7aa28a202bb0a75a44e
dasha.lisikhina@yandex.ru <dasha/lisikhina@yandex.ru>
2022-08-24 21:27:25 +0400

    задача 43

---
+++ задача43/Program.cs
@@ -0,0 +1,9 @@
+﻿// программа, которая находит точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+Console.WriteLine("Введите значение точки b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение точки b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение точки k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение точки k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());

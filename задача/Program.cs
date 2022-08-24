bff9c2712f4b5444b7dbabed6c88e45785646ba6
dasha.lisikhina@yandex.ru <dasha/lisikhina@yandex.ru>
2022-08-24 21:26:28 +0400

    задача дополнительная

---
+++ задача/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте двумерный массив со случайными числами от -10 до 10. Найдите сумму элементов, находящихся на главной диагонали
+int[, ] array = new int[4, 4];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(-10,11);
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+void Sum (int[, ] array)
+{
+    int Sum = 0;
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+    if (i == j)
+    {
+        Sum = Sum + array[i, j];
+    }
+    }
+}
+Console.WriteLine("Сумма " + Sum);
+}
+Sum(array);

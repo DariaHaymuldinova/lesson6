146e48043bb191daf668d5ac9b37dacccadfa51f
dasha.lisikhina@yandex.ru <dasha/lisikhina@yandex.ru>
2022-08-24 20:51:30 +0400

    задача41

---
+++ задача41/Program.cs
@@ -0,0 +1,23 @@
+﻿// Пользователь вводит М чисел, программа считает сколько чисел больше 0 введено
+Console.WriteLine("Введите количество вводимых чисел: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine("Введите число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+void Plus(int[] array)
+{
+   int counter = 0; 
+   foreach (var item in array)
+   {
+    if (item > 0)
+    {
+        counter++;
+    }
+   }
+     Console.WriteLine("Количество чисел больше нуля: " + counter);
+}
+Plus(array);

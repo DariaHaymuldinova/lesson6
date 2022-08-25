2acec0eb4378dcac4b8456137d3837e540e4a316
dasha.lisikhina@yandex.ru <dasha/lisikhina@yandex.ru>
2022-08-25 09:19:40 +0400

    задача43

--- задача43/Program.cs
+++ задача43/Program.cs
@@ -7,3 +7,22 @@ Console.WriteLine("Введите значение точки k1: ");
 int k1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите значение точки k2: ");
 int k2 = Convert.ToInt32(Console.ReadLine());
+void Point (int b1, int b2, int k1, int k2)
+{
+double a = (b2 - b1);
+double b = (k1 - k2);
+double x = a / b;
+double y =  k2 * x + b2;
+Console.WriteLine(y);
+if ((k1 == k2) && (b1 == b2))
+{
+    Console.WriteLine("Прямые совпадают");
+}
+else if (k1 == k2)
+{
+    Console.WriteLine("Прямые паралельны");
+}
+else
+Console.WriteLine($"Точка пересечения ({x};{y})");
+}
+Point(b1,b2,k1,k2);
